e0a922b1350ff14120b021c00350d189595411ad
paul-schultz <paul.william.schultz@gmail.com>
2020-12-17 14:05:45 -0800

    second dataset initialized

--- viz.js
+++ viz.js
@@ -12,47 +12,44 @@ var svg = d3.select("#bar")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
 
+function update(selectedVar) {
 // Parse the Data
 d3.csv("emissions.csv", function(data) {
+    // Add X axis
+    var x = d3.scaleLinear()
+      .domain([0, 140000])
+      .range([ 0, width]);
+    svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x))
+      .selectAll("text")
+        .attr("transform", "translate(-10,0)rotate(-45)")
+        .style("text-anchor", "end");
+  
+    // Y axis
+    var y = d3.scaleBand()
+      .range([ 0, height ])
+      .domain(data.map(function(d) { return d.CompanyName; }))
+      .padding(.1);
+    svg.append("g")
+      .call(d3.axisLeft(y))
+  
+    console.log(data)
+      
+    //Bars
+    var u = svg.selectAll("rect")
+      .data(data)
 
-  // Add X axis
-  var x = d3.scaleLinear()
-    .domain([0, 140000])
-    .range([ 0, width]);
-  svg.append("g")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x))
-    .selectAll("text")
-      .attr("transform", "translate(-10,0)rotate(-45)")
-      .style("text-anchor", "end");
+     u.enter()
+      .append("rect")
+      .merge(u)
+      .attr("x", x(0) )
+      .attr("y", function(d) { return y(d.CompanyName); })
+      .attr("width", function(d) { 
+          return x(d[selectedVar]); })
+      .attr("height", y.bandwidth() )
+      .attr("fill", "#69b3a2")
+  })
+}
 
-  // Y axis
-  var y = d3.scaleBand()
-    .range([ 0, height ])
-    .domain(data.map(function(d) { return d.CompanyName; }))
-    .padding(.1);
-  svg.append("g")
-    .call(d3.axisLeft(y))
-
-  console.log(data)
-    
-  //Bars
-  svg.selectAll("myRect")
-    .data(data)
-    .enter()
-    .append("rect")
-    .attr("x", x(0) )
-    .attr("y", function(d) { return y(d.CompanyName); })
-    .attr("width", function(d) { 
-        return x(d.Scope1Min); })
-    .attr("height", y.bandwidth() )
-    .attr("fill", "#69b3a2")
-
-
-    // .attr("x", function(d) { return x(d.Country); })
-    // .attr("y", function(d) { return y(d.Value); })
-    // .attr("width", x.bandwidth())
-    // .attr("height", function(d) { return height - y(d.Value); })
-    // .attr("fill", "#69b3a2")
-
-})
+update('Scope1Min')

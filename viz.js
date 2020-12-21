8f1a7ac134fd979863038bc5fc528b77757f8fc9
paul-schultz <paul.william.schultz@gmail.com>
2020-12-20 16:58:30 -0800

    init readme files

--- viz.js
+++ viz.js
@@ -1,7 +1,7 @@
 // set the dimensions and margins of the graph
 var margin = {top: 20, right: 30, bottom: 40, left: 150},
     width = 1000 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+    height = 450 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 var svg = d3.select("#bar")
